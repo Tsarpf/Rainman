f42664ebeb85fb49294ad6e6b3c057f831598ed2
Teemu Sarapisto <tsarpf@gmail.com>
2014-07-16 17:36:27 +0300

    Initial commit with very basic stuff done

---
+++ Assets/MoveController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    bool isOnGround = false;
+    int playerSpeed = 10;
+	void Update () {
+	   if(Input.GetKey(KeyCode.A))
+       {
+           gameObject.rigidbody2D.velocity = new Vector2(-playerSpeed, gameObject.rigidbody2D.velocity.y);
+       }
+       else if(Input.GetKey(KeyCode.D))
+       {
+           gameObject.rigidbody2D.velocity = new Vector2(playerSpeed, gameObject.rigidbody2D.velocity.y);
+       }
+       else
+       {
+           gameObject.rigidbody2D.velocity = new Vector2(0, gameObject.rigidbody2D.velocity.y);
+       }
+       if(isOnGround && Input.GetKeyDown(KeyCode.W))
+       {
+           gameObject.rigidbody2D.velocity = new Vector2(gameObject.rigidbody2D.velocity.x, playerSpeed);
+       }
+	}
+
+    void OnCollisionEnter2D()
+    {
+        Debug.Log("enter");
+        isOnGround = true;
+    }
+
+    void OnCollisionExit2D()
+    {
+        Debug.Log("exit");
+        isOnGround = false;
+    }
+
+    void OnCollisionStay2D()
+    {
+        //Debug.Log("stay");
+    }
+}
---
+++ Assets/WorldScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldScript : MonoBehaviour {
+
+	// Use this for initialization
+    Object droplet;
+    System.Random random = new System.Random();
+
+    public float leftPos;
+    public float rightPos;
+    public int scale;
+
+
+	void Start () {
+        droplet = Resources.Load("DropletPrefab");
+        GameObject floor = GameObject.Find("Floor");
+        scale = (int)floor.transform.localScale.x;
+
+        leftPos = scale / 2;
+        rightPos = scale / 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject newthingy = Instantiate(droplet, new Vector3(0,15,0), new Quaternion()) as GameObject;
+        newthingy.transform.position = new Vector2(random.Next(scale) - leftPos, 15);
+	}
+}
