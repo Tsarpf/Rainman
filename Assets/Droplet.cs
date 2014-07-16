b6ca04f4c8e211169d45781a20f7182495b806f1
Tsarpf <tsarpf@gmail.com>
2014-07-16 21:07:55 +0300

    Added splash effect

--- Assets/Droplet.cs
+++ Assets/Droplet.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class Droplet : MonoBehaviour {
 
-	// Use this for initialization
+    float fullSize = 0.75F;
+    float steps;
+    Object splash;
+    
+    // Use this for initialization
 	void Start () {
-	
+        steps = 0.0F;
+        splash = Resources.Load("SplashPrefab");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (steps < fullSize)
+        {
+            steps += 0.01F;
+        }
+        gameObject.transform.localScale = new Vector3(steps, steps, 1);
 	}
 
 
@@ -21,9 +30,27 @@ public class Droplet : MonoBehaviour {
         else if(other.name == "Player")
         {
             Destroy(other.gameObject);
+            gameOver();
+        }
+        else if (other.name == "Floor")
+        {
+            
+            GameObject newSplash = Instantiate(splash) as GameObject;
+            newSplash.transform.position = gameObject.transform.position;  
         }
 
 
         Destroy(gameObject);
     }
+
+    void gameOver()
+    {
+        
+        Object box = Resources.Load("TextBox");
+        WorldScript.GameOver();
+        GameObject tryAgainBox = Instantiate(box) as GameObject;
+        tryAgainBox.transform.position = new Vector3(0.5F, 0, 0);
+
+    }
+
 }
--- Assets/WorldScript.cs
+++ Assets/WorldScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class WorldScript : MonoBehaviour {
 
+    static int dropletsPerFrame;
 	// Use this for initialization
     Object droplet;
     System.Random random = new System.Random();
@@ -13,6 +14,7 @@ public class WorldScript : MonoBehaviour {
 
 
 	void Start () {
+        dropletsPerFrame = 4;
         droplet = Resources.Load("DropletPrefab");
         GameObject floor = GameObject.Find("Floor");
         scale = (int)floor.transform.localScale.x;
@@ -23,7 +25,6 @@ public class WorldScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        int dropletsPerFrame = 4;
         for (int i = 0; i < dropletsPerFrame; i++)
         {
             GameObject newthingy = Instantiate(droplet, new Vector3(0, 15, 0), new Quaternion()) as GameObject;
@@ -32,7 +33,13 @@ public class WorldScript : MonoBehaviour {
             float max = scale;
             float clamped = withDecimals % max;
 
-            newthingy.transform.position = new Vector2(clamped - leftPos, 20);
+            newthingy.transform.position = new Vector2(clamped - leftPos, 25);
         }
 	}
+
+    public static void GameOver()
+    {
+        dropletsPerFrame = 20;
+    }
+
 }
