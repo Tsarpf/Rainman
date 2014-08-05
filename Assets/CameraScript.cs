ea5f4512c3a3343bab96004efaf13a0c646307f8
Tsarpf <tsarpf@gmail.com>
2014-08-06 01:24:13 +0300

    Made the camera only follow the player when he moves forward

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class CameraScript : MonoBehaviour {
 
     GameObject player;
+	float lastPos;
 	// Use this for initialization
 	void Start () {
-        player = GameObject.Find("Player");	
+        player = GameObject.Find("Player");
+		lastPos = player.transform.position.x;
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
         if (player == null) return;
+
+		float currentPos = player.transform.position.x;
+
+		if (currentPos <= lastPos)
+			return;
+
         transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+		lastPos = currentPos;
 	}
 }
